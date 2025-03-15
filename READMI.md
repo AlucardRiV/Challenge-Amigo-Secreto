eda4a26d74490ab44e31352bc6a594f0b6b9de4f
Ian Rivera <ian-dx619@hotmail.com>
2025-03-15 16:04:46 -0600

    Actualizacion en codigo

--- READMI.md
+++ READMI.md
@@ -1,2 +1,6 @@
 <H1> Challenge Amigo Secreto </H1>
 - Estado de proyecto, en construccion
+- Se realiza integracion de GitHub a Visuak Code
+
+<H2> Cambios en codigo </H2>
+- Se revisa tutoriales de Alula de Programacion en JAva para rceordar conceptos de como integrar "valores" a una lista y permita arroja ventaja emergente con mensaje al usaurio
--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+// Array para almacenar los nombres de los amigos
+let amigos = [];
+
+// Aqui agregaremos el nombre del "Amigo" en la lista
+function agregarAmigo() {
+    const input = document.getElementById('amigo');
+    const nombre = input.value.trim();
+
+    if (nombre !== "") {
+        amigos.push(nombre); // Se agrega el "nombre" a la lista
+        input.value = ""; // Se limpia el campo
+        actualizarListaAmigos(); // Se actualiza la lista
+    } else {
+        alert("Por favor, ingresa un nombre válido."); // Alerta cuando el campo esta vacio
+    }
+}
